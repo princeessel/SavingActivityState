fd0b2b5eaa657ff9b86b780cb685c90fe9d64ced
pesselotis <pessel@jimmyjohns.com>
2019-12-22 22:25:09 -0600

    Saving an Activity's state.

---
+++ app/src/main/java/com/example/savingactivitystate/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.savingactivitystate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String KEY_COUNTER = "KEY_COUNTER";
+    private int mCounter = 0;
+    private TextView textView;
+    private Button button;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText =findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView =findViewById(R.id.tv_counter);
+    }
+
+
+
+    public void onCounterClick(View view) {
+        mCounter++;
+        button.setText("Counter:" + mCounter);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNTER, mCounter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCounter = savedInstanceState.getInt(KEY_COUNTER);
+    }
+}
